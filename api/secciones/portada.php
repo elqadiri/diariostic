85cdf3ed26376ea10b652e529532c6ffd8177eeb
elqadiri <153100711+elqadiri@users.noreply.github.com>
2025-10-27 12:48:58 +0100

    Update portada.php with new title and summary

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -1,11 +1,8 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
 
 $portada = [
-"titulo" => "Portada",
-"autor" => "mtorres",
-"resumen" => "El resumen maravilloso",
+    "titulo" => "Portada actualizada", // Ancien titre remplacé par "Portada actualizada"
+    "autor" => "mtorres",
+    "resumen" => "Resumen de la portada",
 ];
 ?>
